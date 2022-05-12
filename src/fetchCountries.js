b85db2695cebcc672788749dc08470af139d6c1a
Pavlo-levitskiy <90378679+Pavlo-levitskiy@users.noreply.github.com>
2022-05-12 11:29:16 +0300

    finish

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,11 @@
-export default function fetchCountrie(name) {
-  return fetch(
-    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`,
-  ).then(response => {
+export default function fetchCountries(name) {
+  const BASE_URL = 'https://restcountries.com/v3.1/name';
+
+  return fetch(`${BASE_URL}/${name}`).then(response => {
+    console.log(response);
+    if (response.status === 404) {
+      return Promise.reject(new Error('ERROR!'));
+    }
     return response.json();
   });
 }
--- src/index.js
+++ src/index.js
@@ -1,58 +1,64 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
-import API from './fetchCountries';
-import debounce from 'lodash.debounce';
-const DEBOUNCE_DELAY = 300;
-let searchId = '';
-//
+import fetchCountries from './fetchCountries';
+
+var debounce = require('lodash.debounce');
+
+const bodyRef = document.querySelector('body');
+
 const refs = {
-  inputEl: document.querySelector('#search-box'),
-  ulEl: document.querySelector('.country-list'),
-  countryInfo: document.querySelector('.country-info'),
+  input: bodyRef.querySelector('#search-box'),
+  ul: bodyRef.querySelector('.country-list'),
+  div: bodyRef.querySelector('.country-info'),
 };
-refs.inputEl.addEventListener('input', debounce(onSearchInput, DEBOUNCE_DELAY));
-//
-function fetchCountries(name) {
-  API(name)
-    .then(info => {
-      renderMarkupUl(info);
-      renderMarkupCountryInfo(info);
-      if (info.length === 1 || info.length <= 10) {
-        Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
-      }
-    })
-    .catch(errorCountry);
-}
-function errorCountry() {
-  Notiflix.Notify.failure('Oops, there is no country with that name');
-}
-//
-function onSearchInput(e) {
-  fetchCountries(searchId);
-  searchId = e.target.value.trim();
-  fetchCountries(searchId);
-}
-//
-function renderMarkupUl(dataList) {
-  const markupUl = dataList
-    .map(item => {
-      console.log(item.name.official);
-      return `<li><img src="${item.flags.svg}" width="50"> ${item.name.official}</li>`;
+
+const DEBOUNCE_DELAY = 300;
+
+const renderList = countries => {
+  if (countries.message) return false;
+  const markup = countries
+    .map(({ flags, name }) => {
+      return `<li><img src="${flags.svg}" alt="${name.official}">${name.official}</li>`;
     })
     .join('');
-  refs.ulEl.innerHTML = markupUl;
-}
-//
-function renderMarkupCountryInfo(country) {
-  const markupCountry = country
-    .map(item => {
-      let language = Object.values(item.languages);
-      return `<ul>
-    <li><span>Capital</span> : ${item.capital}</li>
-    <li><span>Population</span>: ${item.population}</li>
-    <li><span>Languages</span>: ${language}</li>
-    </ul>`;
+  resetTags();
+  refs.ul.innerHTML = markup;
+};
+
+const renderOneCountry = countries => {
+  const markup = countries
+    .map(({ flags, name, capital, population, languages }) => {
+      return `<h2><img src="${flags.svg}" alt="${name.official}">${name.official}</h2>
+        <p><span>Capital:</span>${capital[0]}</p>
+        <p><span>Population:</span>${population}</p>
+        <p><span>Languages :</span>${Object.values(languages).map(lang => lang)}</p>`;
     })
     .join('');
-  refs.countryInfo.innerHTML = markupCountry;
-}
+
+  resetTags();
+  refs.div.innerHTML = markup;
+};
+
+const searchCountry = e => {
+  const serchingCountry = refs.input.value.trim();
+
+  fetchCountries(serchingCountry)
+    .then(countries => {
+      if (countries.length > 10) {
+        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+      } else if (countries.length === 1) renderOneCountry(countries);
+      else renderList(countries);
+    })
+    .catch(error => {
+      console.log(error);
+      resetTags();
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+    });
+};
+
+const resetTags = () => {
+  refs.div.innerHTML = '';
+  refs.ul.innerHTML = '';
+};
+
+refs.input.addEventListener('input', debounce(searchCountry, DEBOUNCE_DELAY));
